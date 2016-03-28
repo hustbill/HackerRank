3b496bda98e11024b43eeb1a4f378d6310fb8ab6
Hua Zhang <hua.zhang@wsu.edu>
2016-03-28 09:25:13 -0700

    Update uds-echo-server.py
    add comments for read_string_from_socket and  write_string_to_socket

--- DistributedSystems/uds-echo-server.py
+++ DistributedSystems/uds-echo-server.py
@@ -1,3 +1,12 @@
+
+## Write your code here
+## This function is called everytime a new connection is accepted by the server
+
+## Use this function to write data to socket
+## write_string_to_socket(connection, message) where connection is the socket object and message is string
+
+## Use this function to read data from socket
+## def read_string_from_socket(connection) where connection is the socket object
 # https://www.hackerrank.com/challenges/uds-echo-server
 
 def write_string_to_socket(connection, message):
@@ -15,7 +24,10 @@ def process_client_connection(connection):
         print "Message received = ", message
         sys.stdout.flush()
 
-       
+        # Your logic goes here
+        # write message back to clinet
+        # write_string_to_socket(connection, message)
+        
         if message == "END":
             break
         write_string_to_socket(connection, message)
